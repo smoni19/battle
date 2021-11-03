96236d7acb5d8cf05371f0bb06a596d54f934dc5
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-11-03 15:34:39 +0000

    added session

--- app.rb
+++ app.rb
@@ -4,14 +4,24 @@ require 'sinatra/reloader'
 class Battle < Sinatra::Base
   register Sinatra::Reloader
   set :sessions, true
+  # Set a session value
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @name_1 = params[:name_1]
-    @name_2 = params[:name_2]
+    session[:name_1] = params['name_1']
+    session[:name_2] = params['name_2']
+    # @name_1 = params[:name_1]
+    # @name_2 = params[:name_2]
+    redirect '/play'
+
+  end
+
+  get '/play'do
+    @name_1 = session[:name_1]
+    @name_2 = session[:name_2]
     erb :play
   end
 
